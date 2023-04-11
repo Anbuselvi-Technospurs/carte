5333d899bff720ec8c54e4f7578da39420145c81
anbuselvi-technospurs <anbuselvi@technospurs.com>
2023-04-11 19:20:12 +0530

    update sample components

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,7 @@
+import * as Styles from "./styles";
+
+const Login = () => {
+  return <Styles.Container>Login</Styles.Container>;
+};
+
+export default Login;
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import LoginBg from "./icons/login_bg.svg";
+
+export const Container = styled.div`
+  height: calc(100vh - 0px);
+  width: 100%;
+  background-size: cover;
+  background-image: url(${LoginBg});
+`;
